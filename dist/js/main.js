b63c26bc90834147dd5ccf12f519ad155a539af5
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-06 16:15:42 +0100

    Initializes voices array from speech synthesis API

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,15 @@ const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#voice-select');
 const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
+
+//Init Voices array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+  console.log(voices);
+};
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
