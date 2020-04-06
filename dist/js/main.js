683cca0063439b77272c4de457c785c788de2bf5
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-06 16:10:43 +0100

    initialized speech synthesis API and DOM elements

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+//Init Speack Synth API
+const synth = window.speechSynthesis;
+
+//DOM Elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#voice-select');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
